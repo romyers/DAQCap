ffed9b31ec62a99248729c91032dd2e5501586c1
myersr252 <myersr252@gmail.com>
2024-07-06 13:38:51 -0400

    Initial commit -- CMake setup and stubs

---
+++ include/DAQCap.h
@@ -0,0 +1,10 @@
+/**
+ * @file DAQCap.h
+ *
+ * @brief Main interface for the DAQCap library.
+ *
+ * @author Robert Myers
+ * Contact: romyers@umich.edu
+ */
+
+#pragma once
---
+++ src/DAQCap.cpp
@@ -0,0 +1,1 @@
+#include <DAQCap.h>
