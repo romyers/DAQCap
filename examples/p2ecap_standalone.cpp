107387e4329efa3ba38b2259cd3ce82f28667e65
myersr252 <myersr252@gmail.com>
2024-07-08 14:08:01 -0400

    Full draft. Not tested. Some more refactoring to do.

--- examples/p2ecap_standalone.cpp
+++ examples/p2ecap_standalone.cpp
@@ -9,13 +9,160 @@
  */
 
 #include <iostream>
+#include <cstring>
+
+#include <sys/stat.h>
 
 #include <DAQCap.h>
 
+DAQCap::Device runDeviceSelection();
+
+bool        directoryExists    (const std::string &path         );
+bool        createDirectory    (const std::string &path         );
+std::string getCurrentTimestamp(const std::string &format       );
+void        createIfMissing    (const std::string &directoryName);
+
 int main(int argc, char **argv) {
 
+    DAQCap::Device device;
+
+    try {
+
+        device = runDeviceSelection();   
+
+    } catch(const std::exception &e) {
+
+        std::cout << e.what() << std::endl;
+
+        exit(1);
+
+    }
+
+    std::cout << "Listening on device: " << device.name << std::endl;
+
+    try {
+
+        DAQCap::SessionHandler handler(device);
+
+        DAQCap::DataBlob blob = handler.fetchPackets();
+
+        std::cout << blob.bufferedPackets << std::endl;
+
+    } catch(const std::exception &e) {
+
+        std::cerr << e.what() << std::endl;
+        std::cout << "Aborted run!" << std::endl;
+
+        exit(1);
+
+    }
+    
+    // Finally, we need to write captured data to a file
     std::cout << "TODO: Implement" << std::endl;
 
     return 0;
+
+}
+
+DAQCap::Device runDeviceSelection() {
+
+    std::vector<DAQCap::Device> devices = DAQCap::getDevices();
+
+    std::cout << std::endl << "Available network devices:" << std::endl;
+    for(int i = 0; i < devices.size(); ++i) {
+
+        std::cout << i + 1 << ": " 
+                  << devices[i].name << "    " 
+                  << devices[i].description << std::endl;
+
+    }
+
+    if(devices.size() == 0) {
+
+        std::cout << "No network devices found. Check your permissions." 
+                  << std::endl;
+        exit(0);
+
+    }
+
+    int deviceNum = -1;
+    do {
+
+        std::cout << "Select a device (1-" << devices.size() << "): ";
+        std::string selection;
+        std::cin >> selection;
+
+        try {
+
+            deviceNum = std::stoi(selection);
+
+        } catch(std::invalid_argument& e) {
+
+            std::cout << "Invalid selection." << std::endl;
+            continue;
+
+        }
+
+        if(deviceNum < 1 || deviceNum > devices.size()) {
+
+            std::cout << "Invalid selection." << std::endl;
+
+        } else {
+
+            break;
+
+        }
+
+    } while(true);
+
+    return devices[deviceNum - 1];
+
+}
+
+bool directoryExists(const std::string &path) {
+
+	struct stat sb;
+
+	if(stat(path.data(), &sb) == 0) {
+
+		return true;
+
+	}
+
+	return false;
+
+}
+
+bool createDirectory(const std::string &path) {
+
+	if(mkdir(path.data(), 0777) == 0) return true;
+
+	return false;
+
+}
+
+std::string getCurrentTimestamp(const std::string &format) {
+
+	char formatBuffer[40];
+	time_t sys_time;
+	struct tm *timeinfo;
+	sys_time = time(0);
+	timeinfo = localtime(&sys_time);
+	std::memset(formatBuffer, 0, sizeof(formatBuffer));
+	strftime(formatBuffer, 40, format.data(), timeinfo);
+
+	return std::string(formatBuffer);
+
+}
+
+void createIfMissing(const std::string &directoryName) {
+
+	if(!directoryExists(directoryName)) {
+
+		createDirectory(directoryName);
+
+		std::cout << "Created output directory: " << directoryName << std::endl;
+
+	}
 
 }
--- include/DAQCap.h
+++ include/DAQCap.h
@@ -10,18 +10,104 @@
 #pragma once
 
 #include <string>
+#include <ostream>
+#include <vector>
 
-struct EthernetDevice {
+// TODO: Can we move this include out?
+#include <pcap.h>
 
-    std::string name;
-    std::string description;
+// TODO: Documentation, especially throws
 
-};
+// TODO: Decide how to handle copy constructor/assignment
 
-class DAQCap {
+namespace DAQCap {
 
-public:
+    /**
+     * @brief Represents a network device on the system.
+     */
+    struct Device {
 
-private:
+        /**
+         * @brief The device name.
+         */
+        std::string name;
 
-};
+        /**
+         * @brief The device description.
+         */
+        std::string description;
+
+    };
+
+    /**
+     * @brief Represents data captured from a network device.
+     */
+    struct DataBlob {
+
+        /**
+         * @brief The number of packets lost while assembling this blob.
+         */
+        int lostPackets = 0;
+
+        /**
+         * @brief The number of packets buffered in this blob.
+         */
+        int bufferedPackets = 0;
+
+        // TODO: Try to get rid of these
+        std::string statusMessage;
+        std::vector<std::string> errorMessages;
+
+        /**
+         * @brief The data stored in this blob.
+         */
+        std::vector<unsigned char> data;
+
+    };
+
+    /**
+     * @brief Provides an interface for managing network devices.
+     */
+    class SessionHandler {
+
+    public:
+
+        SessionHandler(const Device &device, size_t wordSize = 5);
+        ~SessionHandler();
+
+        SessionHandler(const SessionHandler &other)            = delete;
+        SessionHandler &operator=(const SessionHandler &other) = delete;
+
+        /**
+         * @brief Interrupts any currently running calls to fetchPackets().
+         */
+        void interrupt();
+
+        /**
+         * @brief Fetches packets from the session.
+         * 
+         * @param checkPackets If true, the function will check to see if any
+         * packets were lost.
+         * 
+         * @return A DataBlob containing the packets.
+         */
+        DataBlob fetchPackets(bool checkPackets = true);
+
+    private:
+
+        pcap_t *handler;
+
+        int lastPacket;
+
+        size_t wordSize;
+
+    };
+
+    /**
+     * @brief Gets a list of all network devices on the system.
+     * 
+     * @return A vector of Device objects.
+     */
+    std::vector<Device> getDevices();
+
+}
